52ad140455d31353668f8aa6dab1a04d9d5c7c90
GrantDM <mullen.d.grant@gmail.com>
2021-03-31 15:11:03 -0400

    serializers, views, urls made for the API

--- questionbox/views.py
+++ questionbox/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import User, Question, Answer
+from .serializers import UserSerializer, QuestionSerializer, AnswerSerializer
+
 
 # Create your views here.
+
+
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class AnswerList(generics.ListCreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+
+class AnswerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
