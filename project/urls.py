58b2617537e0270cc69a8589f9ecd853856958f1
GrantDM <mullen.d.grant@gmail.com>
2021-04-02 11:58:06 -0400

    nuclear fallout

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
-"""question_box URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,18 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
--- question_box/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuestionBoxConfig(AppConfig):
-    name = 'question_box'
