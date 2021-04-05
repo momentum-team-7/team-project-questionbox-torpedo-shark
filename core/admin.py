ba778cc995b90720e217c757d1b3188276af7530
tristanmatthew <tristanmatthew@gmail.com>
2021-04-05 16:06:44 -0400

    profile model added

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Question, Answer
+from .models import User, Profile,Question, Answer
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Profile)
 admin.site.register(Question)
 admin.site.register(Answer)
