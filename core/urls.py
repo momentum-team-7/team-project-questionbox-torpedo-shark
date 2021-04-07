ba4100c321075f6294be9aa43b524692ab9ba659
GrantDM <mullen.d.grant@gmail.com>
2021-04-07 11:21:47 -0400

    hey it's working

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('user/<int:pk>/', views.UserDetail.as_view()),
     path('questions/', views.QuestionList.as_view()),
     path('questions/<int:pk>/', views.QuestionDetail.as_view()),
-    path('questions/<int:pk>/answers/', views.AnswerList.as_view()),
+    path('questions/<int:question_id>/answers/', views.QuestionResponse.as_view()),
+    path('answers/<int:pk>/', views.AnswerDetail.as_view()),
+    path('answers/', views.AnswerList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
